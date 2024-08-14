7e18524ab8a1396e162e40fd788e68365db6199e
Ganwumeng <ganwumeng@outlook.com>
2024-08-14 16:18:19 +0800

    Initial upload

---
+++ run_model.py
@@ -0,0 +1,41 @@
+import torch
+from transformers import AutoModelForCausalLM, AutoTokenizer
+
+model_name = "Qwen/Qwen2-7B-Instruct-Without-Refusal"
+# model_name = "01-ai/Yi-6B-Chat-Without-Refusal"
+
+model = AutoModelForCausalLM.from_pretrained(
+    model_name,
+    torch_dtype=torch.bfloat16,
+    device_map="cuda"
+).to("cuda")
+
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+while True:
+    prompt = input("> ")
+
+    messages = [
+        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": prompt}
+    ]
+
+    text = tokenizer.apply_chat_template(
+        messages,
+        tokenize=False,
+        add_generation_prompt=True
+    )
+    model_inputs = tokenizer([text], return_tensors="pt").to("cuda")
+
+    generated_ids = model.generate(
+        model_inputs.input_ids,
+        max_new_tokens=128,
+        temperature=0.01
+    )
+    generated_ids = [
+        output_ids[len(input_ids):] for input_ids, output_ids in zip(model_inputs.input_ids, generated_ids)
+    ]
+
+    response = tokenizer.batch_decode(generated_ids, skip_special_tokens=True)[0]
+
+    print(response)
